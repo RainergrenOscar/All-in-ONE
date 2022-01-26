da35288612ca9b183fe5fa2cadaaee519198f3d7
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-26 15:42:21 +0100

    created landingpage and cryptofetch

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Landingpage.scss"
 
 const Landingpage = () => {
-  return <div>lorem</div>;
+  return (
+    <section className='landingsection'>
+      <div className='landingsection__container'>
+      <h1> WELCOME TO ALL IN ONE.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In iusto consectetur, repellendus possimus harum praesentium nihil beatae necessitatibus perferendis? Aut, repellat dignissimos. Perspiciatis, illo voluptatem iusto libero consequatur numquam, dignissimos adipisci dolorem delectus architecto illum incidunt minus, soluta esse. Atque.</p>
+      <div className='landingsection__container__links'>
+        <Link to="/cryptotrackerapi"><button>Crypto Tracker</button></Link>
+        <Link to="/randomfactsapi"><button>Random Facts</button></Link>
+        <Link to="/recipeapi"><button>Recipe</button></Link>
+        </div>
+      </div>
+  </section>)
 };
 
 export default Landingpage;
