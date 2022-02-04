d81c1fd5ee448056a5806c95a173170ace3ce9d6
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-02-04 15:00:24 +0100

    added extra css

--- src/components/apis/crypto/CryptoTrackerApi.js
+++ src/components/apis/crypto/CryptoTrackerApi.js
@@ -22,7 +22,7 @@ const CryptoTrackerApi = () => {
 	})
 
 	return (
-		<section className='cSectios'>
+		<section className='cSection'>
 			<div className='cSection__container'>
 				<div className='cSection__container__split'>
 					<div className='cSection__container__split__left'>
